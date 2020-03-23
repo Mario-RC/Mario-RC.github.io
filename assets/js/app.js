445edd66b5ccd626a6197ab4f01b12b40abafbe2
Mario Rodríguez-Cantelar <mario.rcantelar@gmail.com>
2020-03-23 17:23:27 +0100

    v0.2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,12 @@
+
 function mostrarMenuMobile(e){
 	e.preventDefault();
 	mobileMenu.classList.toggle('is-visible')
 }
 
-
-
 var mobileMenu = document.querySelector('.mobile-menu')
 var triggerMenu = document.querySelectorAll('.trigger-menu')
 
 for( var t of triggerMenu){
-	t.addEventListener('click', mostrarMenuMobile )
+	t.addEventListener('click', mostrarMenuMobile)
 }
