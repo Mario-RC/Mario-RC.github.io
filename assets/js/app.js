ff63ac0af8acc9a522d2ef002383462259c9b70e
Mario Rodríguez-Cantelar <mario.rcantelar@gmail.com>
2020-05-27 01:41:13 +0200

    Added multiple timer techniques

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,15 @@
 
 // MOBILE MENU //
-function mostrarMenuMobile(e) {
-	e.preventDefault();
-	mobileMenu.classList.toggle('is-visible')
+function mostrarMenuMobile(mobile) {
+	mobile.preventDefault();
+	mobileMenu.classList.toggle('is-visible');
 }
 
-var mobileMenu = document.querySelector('.mobile-menu')
-var triggerMenu = document.querySelectorAll('.trigger-menu')
+var mobileMenu = document.querySelector('.mobile-menu');
+var triggerMenu = document.querySelectorAll('.trigger-menu');
 
-for( var t of triggerMenu){
-	t.addEventListener('click', mostrarMenuMobile)
+for(var trigger of triggerMenu) {
+	trigger.addEventListener('click', mostrarMenuMobile);
 }
 
 
@@ -19,17 +19,17 @@ function toggleDark() {
 	element.classList.toggle("dark-mode");
 }
 
-function toggleIcon(x) {
-	x.classList.toggle("fa-sun");
+function toggleIcon(icon) {
+	icon.classList.toggle("fa-sun");
 }
 
 
 // DROPDOWN LANGUAGE MENU //
 var dropdown = document.getElementsByClassName("btn-language");
-for (var tab = 0; tab < dropdown.length; tab++) {
+for(var tab = 0; tab < dropdown.length; tab++) {
   dropdown[tab].addEventListener("click", function() {
     var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
+    if(dropdownContent.style.display === "block") {
       dropdownContent.style.display = "none";
     } else {
       dropdownContent.style.display = "block";
@@ -39,13 +39,18 @@ for (var tab = 0; tab < dropdown.length; tab++) {
 
 
 // TIMER //
+let InitialFocusTime = document.getElementById("focusTime").value*60;
+let InitialRestTime = document.getElementById("restTime").value*60;
+let InitialTechnique = document.getElementById("technique").value;
 let FocusTime = document.getElementById("focusTime").value*60;
 let RestTime = document.getElementById("restTime").value*60;
+let Technique = document.getElementById("technique").value;
 let timeLimit = FocusTime;
 let timeLeft = timeLimit;
 let timePassed = 0;
 let timerInterval = null;
 let onTimesUpChecks = 0;
+let updateInitialTimerValueFlag = 0;
 let onTimesUpFlag = 0;
 let audioCounter = 3;
 
@@ -77,7 +82,11 @@ var check1 = document.getElementById('check1');
 var check2 = document.getElementById('check2');
 var check3 = document.getElementById('check3');
 var check4 = document.getElementById('check4');
-check1.style.display = 'none';
+check1.className;
+check2.className;
+check3.className;
+check4.className;
+check1.style.display = 'inline-flex';
 check3.style.display = 'none';
 check4.style.display = 'none';
 check2.style.display = 'none';
@@ -193,81 +202,233 @@ function calculateTimeLeft(timeLimit, timePassed) {
   return timeLeft = timeLimit - timePassed;
 }
 
-function displayIcon(onTimesUpChecks) {
+function displayIcon(Tecnique, timeLeft, timeLimit, onTimesUpFlag, onTimesUpChecks) {
+  // onTimesUpFlag=1:focus, onTimesUpFlag=1:rest
+  halfTimeLimit = Math.round(timeLimit/2);
+  endTimeLimit = Math.round(timeLimit/8);
+  // Checkmarks State Machine
+  // Check 1 states
   if (onTimesUpChecks === 1) {
-    check1.style.display = "inline-flex";
+    if(onTimesUpFlag === 0) {
+      if(timeLeft === timeLimit) {
+        check1.className = "fas fa-hourglass-start";
+        check1.style.display = "inline-flex";
+      }
+      else if(timeLeft === halfTimeLimit) {
+        check1.className = "fas fa-hourglass-half";
+      }
+      else if(timeLeft === endTimeLimit) {
+        check1.className = "fas fa-hourglass-end";
+      }
+    } else {
+      check1.className = "far fa-check-circle";
+    }
   }
+  // Check 2 states
   else if (onTimesUpChecks === 2) {
-    check2.style.display = "inline-flex";
+    if(onTimesUpFlag === 0) {
+      if(timeLeft === timeLimit) {
+        check2.className = "fas fa-hourglass-start";
+        check2.style.display = "inline-flex";
+      }
+      else if(timeLeft === halfTimeLimit) {
+        check2.className = "fas fa-hourglass-half";
+      }
+      else if(timeLeft === endTimeLimit) {
+        check2.className = "fas fa-hourglass-end";
+      }
+    } else {
+      check2.className = "far fa-check-circle";
+    }
   }
+  // Check 3 states
   else if (onTimesUpChecks === 3) {
-     check3.style.display = "inline-flex";
+    if(onTimesUpFlag === 0) {
+      if(timeLeft === timeLimit) {
+        check3.className = "fas fa-hourglass-start";
+        check3.style.display = "inline-flex";
+      }
+      else if(timeLeft === halfTimeLimit) {
+        check3.className = "fas fa-hourglass-half";
+      }
+      else if(timeLeft === endTimeLimit) {
+        check3.className = "fas fa-hourglass-end";
+      }
+    } else {
+      check3.className = "far fa-check-circle";
+    }
   }
+  // Check 4 states
   else if (onTimesUpChecks === 4) {
-    check4.style.display = "inline-flex";
+    if(onTimesUpFlag === 0) {
+      if(timeLeft === timeLimit) {
+        check4.className = "fas fa-hourglass-start";
+        check4.style.display = "inline-flex";
+      }
+      if(timeLeft === halfTimeLimit) {
+        check4.className = "fas fa-hourglass-half";
+      }
+      else if(timeLeft === endTimeLimit) { 
+        check4.className = "fas fa-hourglass-end";
+      }
+    } else {
+      check4.className = "far fa-check-circle";
+    }
   }
   else {
-    check1.style.display = "none";
-    check2.style.display = "none";
-    check3.style.display = "none";
-    check4.style.display = "none";
+    // Restart checkmarks states
+    if (Tecnique === "pomodoro") {
+      check1.style.display = "inline-flex";
+      check2.style.display = "none";
+      check3.style.display = "none";
+      check4.style.display = "none";
+    } else { // No display checkmarks
+      check1.style.display = "none";
+      check2.style.display = "none";
+      check3.style.display = "none";
+      check4.style.display = "none";
+    }
   }
 }
 
-function startTimer() {
+function getTechniqueTimes() {
+  Tecnique = document.getElementById("technique").value;
   FocusTime = document.getElementById("focusTime").value*60;
   RestTime = document.getElementById("restTime").value*60;
-  timeLimit = FocusTime;
+  return Tecnique, FocusTime, RestTime;
+}
+
+function startTimer() {
+  if (updateInitialTimerValueFlag === 0) {
+    updateInitialTimerValueFlag = 1;
+    Tecnique = document.getElementById("technique").value;
+    FocusTime = document.getElementById("focusTime").value*60;
+    RestTime = document.getElementById("restTime").value*60;
+    timeLimit = FocusTime; // Set initial timer time
+    if (Tecnique === "pomodoro") {
+      onTimesUpChecks = 1;
+    }
+  }
   startBtn.disabled = true;
+
   timerInterval = setInterval(() => {
 
+    // Calculate time and print on timer
     timeLeft = calculateTimeLeft(timeLimit, timePassed)
     printTimer(timeLeft, timeLimit);
     timePassed = timePassed += 1;
 
+    // Stop button
     stopBtn.addEventListener("click", function (event) {
       timePassed = 0;
-      FocusTime = document.getElementById("focusTime").value*60;
-      RestTime = document.getElementById("restTime").value*60;
+      Tecnique, FocusTime, RestTime = getTechniqueTimes();
       timeLimit = FocusTime;
       timeLeft = calculateTimeLeft(timeLimit, timePassed);
       printTimer(timeLeft, timeLimit);
-      pauseTimer();
       onTimesUpFlag = 0;
       onTimesUpChecks = 0;
-      displayIcon(onTimesUpChecks);
+      displayIcon(Tecnique, timeLeft, timeLimit, onTimesUpFlag, onTimesUpChecks)
       startBtn.disabled = false;
-    });
-    
-    pauseBtn.addEventListener("click", function (event) {
+      updateInitialTimerValueFlag = 0;
       pauseTimer();
-      startBtn.disabled = false;
     });
     
+    // Pause button
+    pauseBtn.addEventListener("click", function (event) {
+      startBtn.disabled = false;
+      pauseTimer();
+    });
+    
+    // Timer state machine
+    // End Timer
     if (timeLeft === 0) {
+      // Restart timer count and play times up audio
       timePassed = 0;
       onTimesUpAudio();
+      // Set focus time limit
       if (onTimesUpFlag === 1) {
-        timeLimit = FocusTime;
+        // Switch to rest time limit
         onTimesUpFlag = 0;
+        // If 4 checkmarks, restart checkmarks count
         if (onTimesUpChecks === 4) {
           onTimesUpChecks = 0;
+          displayIcon(Tecnique, timeLeft, timeLimit, onTimesUpFlag, onTimesUpChecks);
         }
-      } else {
-        onTimesUpFlag = 1;
+        // Increase number of checkmarks
         onTimesUpChecks += 1;
+        // Set focus time
+        timeLimit = FocusTime;
+      // Set rest time limit
+      } else {
+        // Switch to focus time limit
+        onTimesUpFlag = 1;
+        // If 4 checkmarks, set long rest time
         if (onTimesUpChecks === 4) {
           timeLimit = RestTime*5;
+        // Set short rest time
         } else {
           timeLimit = RestTime;
         }
       }
-      displayIcon(onTimesUpChecks);
     }
+    displayIcon(Tecnique, timeLeft, timeLimit, onTimesUpFlag, onTimesUpChecks);
 
   }, 1000);
 }
 
+// Start Button
 startBtn.addEventListener("click", function (event) {
   startTimer();
 });
+
+function displayTimerTimes() {
+  InitialTecnique = document.getElementById("technique").value;
+  InitialFocusTime = document.getElementById("focusTime");
+  InitialRestTime = document.getElementById("restTime");
+  if(InitialTecnique === "pomodoro") {
+    InitialFocusTime.value = 25;
+    InitialRestTime.value = 5;
+  }
+  else if(InitialTecnique === "rule 52/17") {
+    InitialFocusTime.value = 52;
+    InitialRestTime.value = 17;
+  }
+  else if(InitialTecnique === "ultradian") {
+    InitialFocusTime.value = 90;
+    InitialRestTime.value = 20;
+  }
+  // Display Updated Timer
+  if(updateInitialTimerValueFlag === 0) {
+    timeLimit = InitialFocusTime.value*60;
+    timeLeft = timeLimit;
+    printTimer(timeLeft, timeLimit);
+    onTimesUpChecks = 0;
+    displayIcon(InitialTecnique, timeLeft, timeLimit, onTimesUpFlag, onTimesUpChecks);
+  }
+}
+
+function displayTechnique() {
+  InitialFocusTime = document.getElementById("focusTime").value;
+  InitialRestTime = document.getElementById("restTime").value;
+  InitialTecnique = document.getElementById("technique");
+  if(InitialFocusTime === "25" && InitialRestTime === "5") {
+    InitialTecnique.value = "pomodoro";
+  }
+  else if(InitialFocusTime === "52" && InitialRestTime === "17") {
+    InitialTecnique.value = "rule 52/17";
+  }
+  else if(InitialFocusTime === "90" && InitialRestTime === "20") {
+    InitialTecnique.value = "ultradian";
+  }
+  else {
+    InitialTecnique.value = "customize";
+  }
+  // Display Updated Timer
+  if(updateInitialTimerValueFlag === 0) {
+    timeLimit = InitialFocusTime*60;
+    timeLeft = timeLimit;
+    printTimer(timeLeft, timeLimit);
+    onTimesUpChecks = 0;
+    displayIcon(InitialTecnique.value, timeLeft, timeLimit, onTimesUpFlag, onTimesUpChecks);
+  }
+}
